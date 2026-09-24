69f8499a98d30995256bced68bcae29ca4e2a68e
Bhanoday777 <bhanodaykurma27@gmail.com>
2026-09-24 20:52:01 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (71.81%) - LeetSync

---
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int,int> mp1;
+        unordered_map<int,int> mp2;
+        bool check=true;
+        for(int i=0;i<arr.size();i++)
+        {
+            mp1[arr[i]]++;
+        }
+        for(auto it:mp1)
+        {
+            mp2[it.second]++;
+        }
+        for(auto it:mp2)
+        {
+            if(it.second==1)
+              continue;
+            else
+               check=false;
+        }
+        return check;
+    }
+};
